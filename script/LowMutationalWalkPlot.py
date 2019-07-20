f577e4e5766456b85dd8ffa83c66749ac01e8e30
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-07-19 21:17:35 -0400

    Revamp mutational walk analysis
    Use FacetGrids!

--- script/LowMutationalWalkPlot.py
+++ script/LowMutationalWalkPlot.py
@@ -11,16 +11,62 @@ import numpy as np
 # open-type fonts
 matplotlib.rcParams['pdf.fonttype'] = 42
 
-df = pd.read_csv(sys.argv[1])
+df_key = pd.read_csv(sys.argv[1])
 
-sns.lineplot(
-    x="Step",
-    y="Match Distance",
-    hue="Metric",
-    data=df
+df_data = pd.read_csv(sys.argv[2])
+
+print("data loaded!")
+
+key = {
+    row['Metric'] : {
+        col : row[col]
+        for col, val in row.iteritems() if col != 'Metric'
+    }
+    for idx, row in df_key.iterrows()
+}
+
+df_data['Dimension'] = df_data.apply(
+    lambda x: key[x['Metric']]['Dimension'],
+    axis=1
 )
 
+df_data['Dimension Type'] = df_data.apply(
+    lambda x: key[x['Metric']]['Dimension Type'],
+    axis=1
+)
+
+df_data['Metric'] = df_data.apply(
+    lambda x: (
+        ('Sliding ' if key[x['Metric']]['Sliding'] else '')
+        + key[x['Metric']]['Base Metric']
+    ),
+    axis=1
+)
+
+print("data crunched!")
+
+g = sns.FacetGrid(
+    df_data[df_data['Dimension Type'] == 'Minimum'],
+    col='Metric',
+    row='Dimension',
+    margin_titles=True
+).set(xlim=(0, 1))
+g.map(sns.lineplot, "Match Score", "Mutational Step")
+
 plt.savefig(
-    "metric-walks.pdf",
+    "minimum-low-mutational-walk.pdf",
+    transparent=True
+)
+
+g = sns.FacetGrid(
+    df_data[df_data['Dimension Type'] == 'Mean'],
+    col='Metric',
+    row='Dimension',
+    margin_titles=True
+).set(xlim=(0, 1))
+g.map(sns.lineplot, "Match Score", "Mutational Step")
+
+plt.savefig(
+    "mean-low-mutational-walk.pdf",
     transparent=True
 )
--- source/LowMutationalWalk.h
+++ source/LowMutationalWalk.h
@@ -18,42 +18,49 @@ void LowMutationalWalk(const Metrics &metrics, const Config &cfg) {
 
   size_t s;
   size_t r;
-  size_t t;
+  size_t step;
   std::string name;
   double match;
 
   emp::DataFile df(cfg.LMW_FILE());
   df.AddVar(s, "Sample");
   df.AddVar(r, "Replicate");
-  df.AddVar(t, "Step");
+  df.AddVar(step, "Mutational Step");
   df.AddVar(name, "Metric");
-  df.AddVar(match, "Match Distance");
+  df.AddVar(match, "Match Score");
   df.PrintHeaderKeys();
 
-  for(s = 0; s < cfg.LMW_NSAMPLES(); ++s) {
-    std::cout << "sample " << s << std::endl;
+  for (const auto & mptr : metrics.mets) {
+    const auto & metric = *mptr;
 
-    const emp::BitSet<32> orig_bs(rand);
+    // filter out non-interesting data
+    if (metric.name().find("Inverse") != std::string::npos) continue;
+    std::cout << "metric " << name << std::endl;
 
-    for(r = 0; r < cfg.LMW_NREPS(); ++r) {
-      std::cout << ".";
-      std::cout.flush();
-      emp::BitSet<32> bs = orig_bs;
 
-      for(t = 0; t < cfg.LMW_NSTEPS(); ++t) {
+    for (s = 0; s < cfg.LMW_NSAMPLES(); ++s) {
 
-        bs.Toggle(rand.GetUInt(32));
+      const emp::BitSet<32> orig_bs(rand);
 
-        for (const auto & mptr : metrics.mets) {
-          const auto & metric = *mptr;
+      for (r = 0; r < cfg.LMW_NREPS(); ++r) {
+
+        emp::BitSet<32> walker(orig_bs);
+
+        for (step = 0; step < cfg.LMW_NSTEPS(); ++step) {
+
+          walker.Toggle(rand.GetUInt(32));
+
           name = metric.name() + " Distance";
-          match = metric(orig_bs, bs);
+          match = metric(orig_bs, walker);
+
           df.Update();
+
         }
 
       }
+
     }
-    std::cout << std::endl;
+
   }
 
 }
