8aa2c9fc2f5c33b28ffe88a2f13a467330ba1214
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-07-23 17:04:15 -0400

    Revamp graph analysis processing script
    Notably, normalize by total graph weight instead of using cutoff

--- script/LowGraphAnalysisProcess.py
+++ script/LowGraphAnalysisProcess.py
@@ -4,30 +4,110 @@ import sys
 from tqdm import tqdm
 import numpy as np
 
-in_df = pd.read_csv(sys.argv[1])
+from keyname import keyname as kn
+from fileshash import fileshash as fsh
+
+dataframe_filename = sys.argv[1]
+
+in_df = pd.read_csv(dataframe_filename)
 out = list()
+out_egv_centralities = list()
+out_load_centralities = list()
 
-for vals, df in tqdm(in_df.groupby(["Metric", "Sample"])):
+for (metric, sample), df in tqdm(in_df.groupby(["Metric", "Sample"])):
 
-    for cutoff in (0.5**exp for exp in range(1,13,4)):
+    G = nx.Graph()
 
-        G = nx.Graph()
+    total_weight = df["Match Score"].sum()
 
-        for idx, row in df.iterrows():
-            if row["Match Distance"] <= cutoff:
-                G.add_edge(row["From"], row["To"])
+    for idx, row in df.iterrows():
+        G.add_edge(
+            row["From"],
+            row["To"],
+            weight=(
+                row["Match Score"] / total_weight
+                if total_weight else 1.0 / len(df)
+            )
+        )
 
-        degrees = np.array([G.degree(n) for n in G.nodes()])
+    egv_cs = nx.katz_centrality(G, weight="weight")
+    load_cs = nx.load_centrality(G, weight="weight")
 
-        out.append({
-            "Metric" : vals[0],
-            "Sample" : vals[1],
-            "Cutoff" : cutoff,
-            "Connected Components" : nx.number_connected_components(G),
-            "Mean Degree" : np.mean(degrees),
-            "Median Degree" : np.median(degrees),
-            "Degree Standard Deviation" : np.std(degrees)
+    egv_cvals = list(egv_cs.values())
+    load_cvals = list(load_cs.values())
+
+    for node, val in egv_cs.items():
+        out_egv_centralities.append({
+            "Metric" : metric,
+            "Sample" : sample,
+            "Node" : node,
+            "Centrality" : val
         })
 
+    for node, val in load_cs.items():
+        out_load_centralities.append({
+            "Metric" : metric,
+            "Sample" : sample,
+            "Node" : node,
+            "Centrality" : val
+        })
 
-pd.DataFrame.from_records(out).to_csv("results.csv", index=False)
+    out.append({
+        "Metric" : metric,
+        "Sample" : sample,
+        "Maximum Eigenvector Centrality" : np.max(egv_cvals),
+        "Eigenvector Centrality Variance" : np.var(egv_cvals),
+        "Maximum Load Centrality" : np.max(load_cvals),
+        "Median Load Centrality" : np.median(load_cvals),
+        "Load Centrality Variance" : np.var(load_cvals),
+        "Minimum Spanning Weight" : nx.minimum_spanning_tree(G).size(weight='weight')
+    })
+
+
+pd.DataFrame.from_records(out).to_csv(
+    kn.pack({
+        'title' : kn.unpack(dataframe_filename)['title'] + "-stats",
+        'bitweight' : kn.unpack(dataframe_filename)['bitweight'],
+        'seed' : kn.unpack(dataframe_filename)['seed'],
+        '_data_hathash_hash' : fsh.FilesHash().hash_files([dataframe_filename]),
+        '_script_fullcat_hash' : fsh.FilesHash(
+                                    file_parcel="full_parcel",
+                                    files_join="cat_join"
+                                ).hash_files([sys.argv[0]]),
+        # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+        'ext' : '.csv'
+    }),
+    index=False
+)
+
+pd.DataFrame.from_records(out_egv_centralities).to_csv(
+    kn.pack({
+        'title' : kn.unpack(dataframe_filename)['title'] + "-eigenvector-centralities",
+        'bitweight' : kn.unpack(dataframe_filename)['bitweight'],
+        'seed' : kn.unpack(dataframe_filename)['seed'],
+        '_data_hathash_hash' : fsh.FilesHash().hash_files([dataframe_filename]),
+        '_script_fullcat_hash' : fsh.FilesHash(
+                                    file_parcel="full_parcel",
+                                    files_join="cat_join"
+                                ).hash_files([sys.argv[0]]),
+        # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+        'ext' : '.csv'
+    }),
+    index=False
+)
+
+pd.DataFrame.from_records(out_load_centralities).to_csv(
+    kn.pack({
+        'title' : kn.unpack(dataframe_filename)['title'] + "-load-centralities",
+        'bitweight' : kn.unpack(dataframe_filename)['bitweight'],
+        'seed' : kn.unpack(dataframe_filename)['seed'],
+        '_data_hathash_hash' : fsh.FilesHash().hash_files([dataframe_filename]),
+        '_script_fullcat_hash' : fsh.FilesHash(
+                                    file_parcel="full_parcel",
+                                    files_join="cat_join"
+                                ).hash_files([sys.argv[0]]),
+        # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+        'ext' : '.csv'
+    }),
+    index=False
+)
