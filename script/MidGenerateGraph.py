d3b581a5a417ceae15842da38277dd3a3f3974b3
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-07-29 23:50:13 -0400

    Add tools to generate graphs with hubs

--- script/MidGenerateGraph.py
+++ script/MidGenerateGraph.py
@@ -4,16 +4,28 @@ import random
 # from matplotlib import pyplot as plt
 from keyname import keyname as kn
 
-seed, nodes, degree, extra = map(int, sys.argv[1:])
+seed, nodes, degree, extra = map(int, sys.argv[1:5])
+
+hubs, hub_degree = map(int, sys.argv[5:7]) if len(sys.argv) == 7 else (0, 0)
 
 random.seed(seed)
 
 G = nx.random_regular_graph(degree, nodes, seed)
 
 # exclude self-loops
-nonedges = list((a, b) for (a,b) in nx.non_edges(G) if a != b)
+nonedges = [(a, b) for (a,b) in nx.non_edges(G) if a != b]
 
 G.add_edges_from( random.sample(nonedges, extra) )
+
+# use first hubs nodes as hubs
+for node in range(hubs):
+    G.add_edges_from([
+        (node, neigh)
+        for neigh in random.sample(
+            [n for n in nx.non_neighbors(G, node) if n != node],
+            max(0, hub_degree - G.degree(node))
+        )
+    ])
 
 # nx.draw(G)
 # plt.show()
@@ -24,6 +36,8 @@ outfile = kn.pack({
     'node-count' : nodes,
     'base-degree' : degree,
     'extra-edges' : extra,
+    'hub-count' : hubs,
+    'hub-degree' : hub_degree,
     # '_script_fullcat_hash' : fsh.FilesHash(
     #                             file_parcel="full_parcel",
     #                             files_join="cat_join"
