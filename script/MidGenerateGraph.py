308ed7478e44e3ac1d77e2e1a73d956c9352ed59
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-07-30 15:29:27 -0400

    Generate graphs with distinct components

--- script/MidGenerateGraph.py
+++ script/MidGenerateGraph.py
@@ -6,26 +6,38 @@ from keyname import keyname as kn
 
 seed, nodes, degree, extra = map(int, sys.argv[1:5])
 
-hubs, hub_degree = map(int, sys.argv[5:7]) if len(sys.argv) == 7 else (0, 0)
+hubs, hub_degree = map(int, sys.argv[5:7]) if len(sys.argv) > 6 else (0, 0)
+
+num_components = int(sys.argv[7]) if len(sys.argv) > 7 else 1
+
+print(num_components)
 
 random.seed(seed)
 
-G = nx.random_regular_graph(degree, nodes, seed)
+components = []
 
-# exclude self-loops
-nonedges = [(a, b) for (a,b) in nx.non_edges(G) if a != b]
+for __ in range(num_components):
 
-G.add_edges_from( random.sample(nonedges, extra) )
+    G = nx.random_regular_graph(degree, nodes, random.randrange(2e32))
 
-# use first hubs nodes as hubs
-for node in range(hubs):
-    G.add_edges_from([
-        (node, neigh)
-        for neigh in random.sample(
-            [n for n in nx.non_neighbors(G, node) if n != node],
-            max(0, hub_degree - G.degree(node))
-        )
-    ])
+    # exclude self-loops
+    nonedges = [(a, b) for (a,b) in nx.non_edges(G) if a != b]
+
+    G.add_edges_from( random.sample(nonedges, extra) )
+
+    # use first hubs nodes as hubs
+    for node in range(hubs):
+        G.add_edges_from([
+            (node, neigh)
+            for neigh in random.sample(
+                [n for n in nx.non_neighbors(G, node) if n != node],
+                max(0, hub_degree - G.degree(node))
+            )
+        ])
+
+    components.append(G)
+
+G = nx.disjoint_union_all(components)
 
 # nx.draw(G)
 # plt.show()
@@ -33,7 +45,8 @@ for node in range(hubs):
 outfile = kn.pack({
     'seed' : seed,
     'title' : 'mid-graph',
-    'node-count' : nodes,
+    'node-count' : nodes * num_components,
+    'nodes-per-component' : nodes,
     'base-degree' : degree,
     'extra-edges' : extra,
     'hub-count' : hubs,
