a5069d41f86ded8d4ce1ed124728ffbc6999d2d2
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-07 22:35:27 -0400

    Update plotting script

--- script/MidFlexMatchFitnessPlot.py
+++ script/MidFlexMatchFitnessPlot.py
@@ -27,11 +27,16 @@ dfs = [
 
 print("Data loaded!")
 
+res = []
 for filename, df in dfs:
+
+    # assumes datafiles are each from one replicate
     for k, v in kn.unpack(filename).items():
         df[k] = v
 
-df_data = pd.concat([df for filename, df in dfs])
+    res.append(df)
+
+df_data = pd.concat(res)
 
 df_data['Slug'] = df_data['metric-slug']
 
@@ -65,25 +70,17 @@ df_data['Inverse'] = df_data.apply(
 
 df_data['Metric'] = df_data.apply(
     lambda x: (
-        ('Sliding ' if key[x['Slug']]['Sliding'] else '')
+        ('Started ' if 'Started' in x['Metric'] else '')
         + key[x['Slug']]['Base Metric']
-    ),
-    axis=1
-)
-
-df_data['Metric'] = df_data.apply(
-    lambda x: (
-        ('Inverse ' if x['Inverse'] else '')
-        + x['Metric']
     ),
     axis=1
 )
 
 df_data['Treatment'] = df_data['treatment']
 
-df_data['Best Fitness'] = df_data['max_fitness']
+df_data['Update'] = df_data['update']
 
-df_data['Generation'] = df_data['update']
+df_data['Maximum Fitness'] = df_data['max_fitness']
 
 df_data['Dimension Count'] = df_data['Dimension']
 
@@ -91,102 +88,79 @@ df_data['Dimension'] = df_data.apply(
     lambda x: x['Dimension Type'] + " " + str(x['Dimension']),
     axis=1
 )
+
+df_data['Target Size'] = df_data['target-size']
+
+df_data['Target Configuration'] = df_data['target-config']
+
 print("Data crunched!")
 
-g = sns.FacetGrid(
-    df_data,
-    col='Metric',
-    row='Dimension',
-    hue='Treatment',
-    hue_kws={
-        'ls' : list(itertools.islice(
-            itertools.cycle(['-', '--', '-.', ':']),
-            len(df_data['Treatment'].unique())
-        )),
-        'color' : sns.color_palette()
-    },
-    margin_titles=True,
-    col_order=(
-        sorted(
-            [x for x in df_data['Metric'].unique() if 'Inverse' in x]
-        ) + sorted(
-            [x for x in df_data['Metric'].unique() if 'Inverse' not in x],
-        )
-    ),
-    row_order=(
-        sorted(
-            [x for x in df_data['Dimension'].unique() if 'Mean' in x],
-            key=lambda str: next(int(s) for s in str.split() if s.isdigit())
-        ) + sorted(
-            [x for x in df_data['Dimension'].unique() if 'Minimum' in x],
-            key=lambda str: next(int(s) for s in str.split() if s.isdigit())
-        ) + sorted(
-            [x for x in df_data['Dimension'].unique() if 'Euclidean' in x],
-            key=lambda str: next(int(s) for s in str.split() if s.isdigit())
-        )
-    )
-).set(ylim=(0, 1))
-g.map(
-    sns.lineplot,
-    'Generation',
-    'Best Fitness',
-    style_order=list(df_data['Treatment'].unique())
-).add_legend()
-
-assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
-assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
-assert len({kn.unpack(f)['fit-fun'] for f in dataframe_filenames}) == 1
-
-outfile = kn.pack({
-    'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
-    'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
-    'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
-    'viz' : 'history',
-    '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
-    '_script_fullcat_hash' : fsh.FilesHash(
-                                file_parcel="full_parcel",
-                                files_join="cat_join"
-                            ).hash_files([sys.argv[0]]),
-    # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
-    'ext' : '.pdf'
-})
-plt.savefig(
-    outfile,
-    transparent=True,
-    bbox_inches='tight',
-    pad_inches=0
-)
-print("output saved to", outfile)
+# g = sns.FacetGrid(
+#     df_data,
+#     col='Target Configuration',
+#     row='Target Size',
+#     hue='Metric',
+#     hue_kws={
+#         'ls' : list(itertools.islice(
+#             itertools.cycle(['-', '--', '-.', ':']),
+#             len(df_data['Metric'].unique())
+#         )),
+#         'color' : sns.color_palette()
+#     },
+#     margin_titles=True
+# ).set(ylim=(0, 1))
+# g.map(
+#     sns.lineplot,
+#     'Update',
+#     'Maximum Fitness',
+#     style_order=list(df_data['Metric'].unique())
+# ).add_legend()
+#
+# assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
+# assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
+# assert len({kn.unpack(f)['fit-fun'] for f in dataframe_filenames}) == 1
+#
+# outfile = kn.pack({
+#     'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
+#     'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
+#     'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
+#     'viz' : 'fitness-line',
+#     '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
+#     '_script_fullcat_hash' : fsh.FilesHash(
+#                                 file_parcel="full_parcel",
+#                                 files_join="cat_join"
+#                             ).hash_files([sys.argv[0]]),
+#     # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+#     'ext' : '.pdf'
+# })
+# plt.savefig(
+#     outfile,
+#     transparent=True,
+#     bbox_inches='tight',
+#     pad_inches=0
+# )
+# print("output saved to", outfile)
+#
+# plt.clf()
 
 g = sns.FacetGrid(
-    df_data[df_data["Generation"] == df_data["Generation"].max()],
-    col='Metric',
-    row='Dimension',
-    hue='Treatment',
-    margin_titles=True,
-    col_order=(
-        sorted(
-            [x for x in df_data['Metric'].unique() if 'Inverse' in x]
-        ) + sorted(
-            [x for x in df_data['Metric'].unique() if 'Inverse' not in x],
-        )
-    ),
-    row_order=(
-        sorted(
-            [x for x in df_data['Dimension'].unique() if 'Mean' in x],
-            key=lambda str: next(int(s) for s in str.split() if s.isdigit())
-        ) + sorted(
-            [x for x in df_data['Dimension'].unique() if 'Minimum' in x],
-            key=lambda str: next(int(s) for s in str.split() if s.isdigit())
-        )
-    )
+    df_data.groupby([
+        'seed',
+        'Metric',
+        'Target Configuration',
+        'Target Size'
+    ]).agg('max').reset_index(),
+    col='Target Configuration',
+    row='Target Size',
+    # hue='Metric',
+    margin_titles=True
 ).set(ylim=(0, 1))
 g.map(
     sns.barplot,
-    'Treatment',
-    'Best Fitness',
-    order=list(df_data["Treatment"].unique())
+    'Metric',
+    'Maximum Fitness',
 ).add_legend()
+g.set_xticklabels(rotation=-90)
 
 assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
 assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
@@ -196,7 +170,7 @@ outfile = kn.pack({
     'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
     'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
     'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
-    'viz' : 'endstate',
+    'viz' : 'fitness-max',
     '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
     '_script_fullcat_hash' : fsh.FilesHash(
                                 file_parcel="full_parcel",
@@ -212,3 +186,5 @@ plt.savefig(
     pad_inches=0
 )
 print("output saved to", outfile)
+
+plt.clf()
