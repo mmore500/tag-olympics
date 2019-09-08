d6c31de134fc7ebd12a366275072133371fb255a
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-07 22:33:09 -0400

    Add target config and size keyname info

--- script/MidFlexMatchNeutralityLinePlot.py
+++ script/MidFlexMatchNeutralityLinePlot.py
@@ -85,31 +85,98 @@ df_data['Dimension'] = df_data.apply(
     axis=1
 )
 
+df_data['Target Size'] = df_data['target-size']
+
+df_data['Target Configuration'] = df_data['target-config']
+
 print("Data crunched!")
 
-measure = 'Genetic Distance'
-res = sns.lineplot(
-    'Step',
-    'Value',
-    hue='Metric',
-    data=df_data[
-        (df_data['Measure'] == measure)
+df_data['Genetic Distance'] = df_data['Value']
+
+g = sns.FacetGrid(
+    df_data[
+        (df_data['Measure'] == 'Genetic Distance')
         & (df_data['Statistic'] == 'Median')
-    ]
-)
-res.set(ylabel='Median ' + measure)
+    ],
+    col='Target Configuration',
+    row='Target Size',
+    hue='Metric',
+    hue_kws={
+        'ls' : list(itertools.islice(
+            itertools.cycle(['-', '--', '-.', ':']),
+            len(df_data['Metric'].unique())
+        )),
+        'color' : sns.color_palette()
+    },
+    margin_titles=True
+).set(ylim=(0, 1))
+g.map(
+    sns.lineplot,
+    'Step',
+    'Genetic Distance',
+    style_order=list(df_data['Metric'].unique())
+).add_legend()
 
-plt.setp(res.get_xticklabels(), rotation=45)
-
-assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
 assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
 assert len({kn.unpack(f)['fit-fun'] for f in dataframe_filenames}) == 1
 
 outfile = kn.pack({
-    'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
     'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
     'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
     'viz' : 'neutrality-ancestry-line',
+    '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
+    '_script_fullcat_hash' : fsh.FilesHash(
+                                file_parcel="full_parcel",
+                                files_join="cat_join"
+                            ).hash_files([sys.argv[0]]),
+    # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+    'ext' : '.pdf'
+})
+plt.savefig(
+    outfile,
+    transparent=True,
+    bbox_inches='tight',
+    pad_inches=0
+)
+print("output saved to", outfile)
+
+plt.clf()
+
+g = sns.FacetGrid(
+    data=df_data[
+        (df_data['Statistic'] == 'Median')
+    ].pivot_table(
+        index=['Step', 'Metric', 'seed', 'Target Configuration', 'Target Size'],
+        columns='Measure',
+        values='Value',
+        aggfunc='first'
+    ).reset_index(),
+    col='Target Configuration',
+    row='Target Size',
+    hue='Metric',
+    hue_kws={
+        'ls' : list(itertools.islice(
+            itertools.cycle(['-', '--', '-.', ':']),
+            len(df_data['Metric'].unique())
+        )),
+        'color' : sns.color_palette()
+    },
+    margin_titles=True
+).set(ylim=(0, 1))
+g.map(
+    sns.lineplot,
+    'Updates Elapsed',
+    'Genetic Distance',
+    style_order=list(df_data['Metric'].unique())
+).add_legend()
+
+assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
+assert len({kn.unpack(f)['fit-fun'] for f in dataframe_filenames}) == 1
+
+outfile = kn.pack({
+    'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
+    'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
+    'viz' : 'distance-update',
     '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
     '_script_fullcat_hash' : fsh.FilesHash(
                                 file_parcel="full_parcel",
--- source/Config.h
+++ source/Config.h
@@ -121,5 +121,6 @@ EMP_BUILD_CONFIG(
     "How many often to output data while stepping back through systematics."
   ),
   VALUE(MFM_TITLE, std::string, "mid-flex-match", "Default out filename")
+  VALUE(MFM_TARGET_CONFIG, std::string, "unspecified", "How are target nodes connected to one another?")
 
 )
