9cf77b5d74500f59f4602af65337e552adfdd0d1
Matthew Andres Moreno <mmore500.login@gmail.com>
2019-09-09 20:21:38 -0400

    Add missing plot, label x axes the right way

--- script/MidFlexMatchNeutralityLinePlot.py
+++ script/MidFlexMatchNeutralityLinePlot.py
@@ -91,8 +91,6 @@ df_data['Target Configuration'] = df_data['target-config']
 
 print("Data crunched!")
 
-df_data['Genetic Distance'] = df_data['Value']
-
 g = sns.FacetGrid(
     df_data[
         (df_data['Measure'] == 'Genetic Distance')
@@ -113,9 +111,9 @@ g = sns.FacetGrid(
 g.map(
     sns.lineplot,
     'Step',
-    'Genetic Distance',
+    'Value',
     style_order=list(df_data['Metric'].unique())
-).add_legend()
+).add_legend().set_xlabels("Genetic Distance")
 
 assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
 assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
@@ -179,7 +177,58 @@ outfile = kn.pack({
     'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
     'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
     'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
-    'viz' : 'distance-update',
+    'viz' : 'neutrality-distance-update',
+    '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
+    '_script_fullcat_hash' : fsh.FilesHash(
+                                file_parcel="full_parcel",
+                                files_join="cat_join"
+                            ).hash_files([sys.argv[0]]),
+    # '_source_hash' :kn.unpack(dataframe_filename)['_source_hash'],
+    'ext' : '.pdf'
+})
+plt.savefig(
+    outfile,
+    transparent=True,
+    bbox_inches='tight',
+    pad_inches=0
+)
+print("output saved to", outfile)
+
+plt.clf()
+
+g = sns.FacetGrid(
+    df_data[
+        (df_data['Measure'] == 'Updates Elapsed')
+        & (df_data['Statistic'] == 'Median')
+    ],
+    col='Target Configuration',
+    row='Target Size',
+    hue='Metric',
+    hue_kws={
+        'ls' : list(itertools.islice(
+            itertools.cycle(['-', '--', '-.', ':']),
+            len(df_data['Metric'].unique())
+        )),
+        'color' : sns.color_palette()
+    },
+    margin_titles=True
+).set(ylim=(0, 1))
+g.map(
+    sns.lineplot,
+    'Value',
+    'Step',
+    style_order=list(df_data['Metric'].unique())
+).add_legend().set_xlabels("Genetic Distance")
+
+assert len({kn.unpack(f)['experiment'] for f in dataframe_filenames}) == 1
+assert len({kn.unpack(f)['bitweight'] for f in dataframe_filenames}) == 1
+assert len({kn.unpack(f)['fit-fun'] for f in dataframe_filenames}) == 1
+
+outfile = kn.pack({
+    'experiment' : kn.unpack(dataframe_filenames[0])['experiment'],
+    'bitweight' : kn.unpack(dataframe_filenames[0])['bitweight'],
+    'fit-fun' : kn.unpack(dataframe_filenames[0])['fit-fun'],
+    'viz' : 'neutrality-step-update',
     '_data_hathash_hash' : fsh.FilesHash().hash_files(dataframe_filenames),
     '_script_fullcat_hash' : fsh.FilesHash(
                                 file_parcel="full_parcel",
