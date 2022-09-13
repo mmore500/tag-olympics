5feec9d20694750b69998d5a2c56fce8be374674
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-09-12 19:26:02 -0700

    Put hamming and streak metrics together

--- pylib/_lookup_metric_color.py
+++ pylib/_lookup_metric_color.py
@@ -4,8 +4,8 @@ def lookup_metric_color(metric_name: str) -> str:
     colors = {
         'Hash' : 'C0',
         'Hamming' : 'C1',
-        'Integer' : 'C2',
-        'Integer (bi)' : 'C3',
-        'Streak' : 'C4',
+        'Streak' : 'C2',
+        'Integer' : 'C3',
+        'Integer (bi)' : 'C4',
     }
     return colors[metric_name]
--- pylib/_lookup_metric_priority.py
+++ pylib/_lookup_metric_priority.py
@@ -4,9 +4,9 @@ def lookup_metric_priority(metric_name: str) -> int:
     priority_order = [
         'Hash', # put hash first because it is control
         'Hamming',
+        'Streak',
         'Integer',
         'Integer (bi)',
-        'Streak',
     ]
 
     return priority_order.index(metric_name)
